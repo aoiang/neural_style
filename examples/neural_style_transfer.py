9a7f5ea273d11bd72846efa876dd3de7f377a576
aoiang <zyyaoiang@gmail.com>
2018-11-05 22:13:04 +0800

    update

--- examples/neural_style_transfer.py
+++ examples/neural_style_transfer.py
@@ -211,21 +211,68 @@ def total_variation_loss(x):
             x[:, :img_nrows - 1, :img_ncols - 1, :] - x[:, :img_nrows - 1, 1:, :])
     return K.sum(K.pow(a + b, 1.25))
 
+# this Evaluator class makes it possible
+# to compute loss and gradients in one pass
+# while retrieving them via two separate functions,
+# "loss" and "grads". This is done because scipy.optimize
+# requires separate functions for loss and gradients,
+# but computing them separately would be inefficient.
+class Evaluator(object):
+
+    def __init__(self):
+        self.loss_value = None
+        self.grads_values = None
+
+    def loss(self, x):
+        assert self.loss_value is None
+        loss_value, grad_values = eval_loss_and_grads(x)
+        self.loss_value = loss_value
+        self.grad_values = grad_values
+        return self.loss_value
+
+    def grads(self, x):
+        assert self.loss_value is not None
+        grad_values = np.copy(self.grad_values)
+        self.loss_value = None
+        self.grad_values = None
+        return grad_values
+
+
+def eval_loss_and_grads(x):
+    if K.image_data_format() == 'channels_first':
+        x = x.reshape((1, 3, img_nrows, img_ncols))
+    else:
+        x = x.reshape((1, img_nrows, img_ncols, 3))
+    outs = f_outputs([x])
+    loss_value = outs[0]
+    if len(outs[1:]) == 1:
+        grad_values = outs[1].flatten().astype('float64')
+    else:
+        grad_values = np.array(outs[1:]).flatten().astype('float64')
+    return loss_value, grad_values
+
+
+
 # combine these loss functions into a single scalar
+content_features = ['reduction_concat_stem_2']
+# content_features = ['block4_conv2', 'block5_conv2']
 loss = K.variable(0.)
-layer_features = outputs_dict['normal_concat_15']
-# layer_features = outputs_dict['block5_conv2']
-base_image_features = layer_features[0, :, :, :]
-combination_features = layer_features[2, :, :, :]
-loss += content_weight * content_loss(base_image_features,
-                                      combination_features)
+for layer_name in content_features:
+    layer_features = outputs_dict[layer_name]
+    base_image_features = layer_features[0, :, :, :]
+    combination_features = layer_features[2, :, :, :]
+    loss += content_weight * content_loss(base_image_features,
+                                          combination_features)
 
 # feature_layers = ['block1_conv1', 'block2_conv1',
 #                   'block3_conv1', 'block4_conv1',
 #                   'block5_conv1']
-feature_layers = ['normal_concat_10', 'normal_concat_11',
-                  'normal_concat_12', 'normal_concat_13',
-                  'normal_concat_14']
+# feature_layers = ['normal_concat_1', 'reduction_concat_reduce_6',
+#                   'normal_concat_2', 'normal_concat_3',
+#                   'normal_concat_4', 'normal_concat_5']
+
+feature_layers = ['reduction_concat_stem_1']
+# feature_layers = ['block3_conv1']
 for layer_name in feature_layers:
     layer_features = outputs_dict[layer_name]
     style_reference_features = layer_features[1, :, :, :]
@@ -247,46 +294,6 @@ else:
 f_outputs = K.function([combination_image], outputs)
 
 
-def eval_loss_and_grads(x):
-    if K.image_data_format() == 'channels_first':
-        x = x.reshape((1, 3, img_nrows, img_ncols))
-    else:
-        x = x.reshape((1, img_nrows, img_ncols, 3))
-    outs = f_outputs([x])
-    loss_value = outs[0]
-    if len(outs[1:]) == 1:
-        grad_values = outs[1].flatten().astype('float64')
-    else:
-        grad_values = np.array(outs[1:]).flatten().astype('float64')
-    return loss_value, grad_values
-
-# this Evaluator class makes it possible
-# to compute loss and gradients in one pass
-# while retrieving them via two separate functions,
-# "loss" and "grads". This is done because scipy.optimize
-# requires separate functions for loss and gradients,
-# but computing them separately would be inefficient.
-
-
-class Evaluator(object):
-
-    def __init__(self):
-        self.loss_value = None
-        self.grads_values = None
-
-    def loss(self, x):
-        assert self.loss_value is None
-        loss_value, grad_values = eval_loss_and_grads(x)
-        self.loss_value = loss_value
-        self.grad_values = grad_values
-        return self.loss_value
-
-    def grads(self, x):
-        assert self.loss_value is not None
-        grad_values = np.copy(self.grad_values)
-        self.loss_value = None
-        self.grad_values = None
-        return grad_values
 
 evaluator = Evaluator()
 
